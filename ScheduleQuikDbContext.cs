5a7961dd1f41366aa80a421fb12c370cc534d142
Frank Diaz <diazfrankp@gmail.com>
2018-12-31 15:33:31 -0500

    Created EmployeesTable.cs

--- ScheduleQuikDbContext.cs
+++ ScheduleQuikDbContext.cs
@@ -4,29 +4,28 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace ScheduleQuikWebServer
 {
-    public partial class ScheduleQuikDbContext : DbContext
+  public partial class ScheduleQuikDbContext : DbContext
+  {
+    public ScheduleQuikDbContext()
     {
-        public ScheduleQuikDbContext()
-        {
-        }
-
-        public ScheduleQuikDbContext(DbContextOptions<ScheduleQuikDbContext> options)
-            : base(options)
-        {
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseNpgsql("server=localhost;database=ScheduleQuikDb");
-            }
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
-        }
     }
+
+    public ScheduleQuikDbContext(DbContextOptions<ScheduleQuikDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+        optionsBuilder.UseNpgsql("server=localhost;database=ScheduleQuikDb");
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+    }
+  }
 }
