13122df322dcb68da765e511482ddafce947598e
Frank Diaz <diazfrankp@gmail.com>
2018-12-31 15:58:39 -0500

    Created EmployeeController and GET request /api/employee by FirstName.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ScheduleQuikWebServer.Models;
+
+namespace ScheduleQuikWebServer.Controllers
+{
+  [Route("api/[controller]")]
+  // localhost:5001/api/values
+  [ApiController]
+  public class EmployeesController : ControllerBase
+  {
+    //GET that returns all employees
+    //GET /api/employee
+    [HttpGet]
+
+    public ActionResult<IEnumerable<EmployeesTable>> GetAction()
+    {
+      // query my database
+      var db = new ScheduleQuikDbContext();
+      var results = db.Employee.OrderBy(employee => employee.FirstName);
+      // return the results
+      return results.ToList();
+
+    }
+  }
+}
