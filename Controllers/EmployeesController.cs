7ba8752f4f4ce39d3281814ed591c03bf92a4a83
Frank Diaz <diazfrankp@gmail.com>
2018-12-31 16:38:22 -0500

    Added HttpPost for AddEmployees.

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -13,17 +13,25 @@ namespace ScheduleQuikWebServer.Controllers
   public class EmployeesController : ControllerBase
   {
     //GET that returns all employees
-    //GET /api/employee
+    //GET /api/employees
     [HttpGet]
 
     public ActionResult<IEnumerable<EmployeesTable>> GetAction()
     {
       // query my database
       var db = new ScheduleQuikDbContext();
-      var results = db.Employees.OrderBy(employee => employee.FirstName);
+      var employee = db.Employees.OrderBy(employees => employees.FirstName);
       // return the results
-      return results.ToList();
-
+      return employee.ToList();
+      //should I update this to OrderBy(employees => employees.FirstName || employees.LastName);  ?
+    }
+    [HttpPost]
+    public ActionResult<EmployeesTable> AddEmployees([FromBody] EmployeesTable incomingEmployees)
+    {
+      var db = new ScheduleQuikDbContext();
+      db.Employees.Add(incomingEmployees);
+      db.SaveChanges();
+      return incomingEmployees;
     }
   }
 }
